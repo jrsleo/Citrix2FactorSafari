30b396610fc883f893ee3727d054e9f132d9d4a3
Jordan Schultz <jordan@JordanSchultzs-MacBook-Pro.local>
2015-10-21 16:52:37 -0400

    refactored code for parsing out input code

--- challenge.js
+++ challenge.js
@@ -4,18 +4,13 @@ document.getElementById('lblChallenge').addEventListener('onload',
     parse(question));
 
 function parse (input) {
-	input = input.trim();
 	var output = new Array();
-
+	input = input.trim();
 	input = input.split(" ");
 
 	for (var i = 0; i < input.length; i++) {
-		var temp = new Array();
-
 		if (input[i].charAt(0) == "[") {
-			temp[i] = input[i].slice(1,3);
-
-			output.push(temp[i]);
+			output.push(input[i].slice(1,3));
 		};
 	};
 
